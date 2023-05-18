bc90af6df1e92906e51639fc0def6c2a473b87cc
Khanh Nguyen Dinh <nguyendinhkhanh1997@gmail.com>
2023-05-18 17:15:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
-const express = require("express");
-const app = express();
-const product = require("./api/product");
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
-app.use(express.json({ extended: false }));
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use("/api/product", product);
+app.get('/image-urls', (req, res) => {
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+    console.log(req);
+
+    const urls = [
+        'https://lux.mrcong.com/images/2023/04/26/DJAWA-Photo-Son-Ye-Eun-Early-Spring-Walk-in-March-Vol.2-S.Ver-MrCong.com-001.webp',
+        'https://lux.mrcong.com/images/2023/04/26/DJAWA-Photo-Son-Ye-Eun-Early-Spring-Walk-in-March-Vol.2-S.Ver-MrCong.com-002.webp',
+        'https://lux.mrcong.com/images/2023/04/26/DJAWA-Photo-Son-Ye-Eun-Early-Spring-Walk-in-March-Vol.2-S.Ver-MrCong.com-999.webp',
+    ]
+    res.render('image-urls', { urls })
+})
+
+app.post('/submit-text', (req, res) => {
+    const text = req.body.text
+    const urls = [];
+    for (let index = 0; index < 999; index++) {
+
+        urls.push(getUrl(text, index));
+    }
+    // const urls = [
+    //     'https://lux.mrcong.com/images/2023/04/26/DJAWA-Photo-Son-Ye-Eun-Early-Spring-Walk-in-March-Vol.2-S.Ver-MrCong.com-001.webp',
+    //     'https://lux.mrcong.com/images/2023/04/26/DJAWA-Photo-Son-Ye-Eun-Early-Spring-Walk-in-March-Vol.2-S.Ver-MrCong.com-002.webp',
+    //     'https://lux.mrcong.com/images/2023/04/26/DJAWA-Photo-Son-Ye-Eun-Early-Spring-Walk-in-March-Vol.2-S.Ver-MrCong.com-999.webp',
+    // ]
+    res.render('image-urls', { urls })
+    //res.send(`You submitted: ${text}`)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+app.listen(8080, () => {
+    console.log('Server listening on port 3000')
+})
+
+function getUrl(rawUrl, index) {
+    let i = index;
+    if (index < 10) {
+        i = '00' + index;
+    }
+    else if (index < 100) {
+        i = '0' + index;
+    }
+    let url = rawUrl.replace('001', i);
+    return url;
+
+}
